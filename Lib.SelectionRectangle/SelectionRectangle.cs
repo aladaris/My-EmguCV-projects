1518ac084cbfd88ecf670421ebb7a5f2233459d2
Fernando González López - Peñalver <aladaris@gmail.com>
2014-01-13 13:49:59 +0000

    Añadido fichero con la licencia (GPL V3).
    Añadida cabecera de autoría y licenia a los ficheros de código.

--- Lib.SelectionRectangle/SelectionRectangle.cs
+++ Lib.SelectionRectangle/SelectionRectangle.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+   Copyright © 2014 Fernando González López - Peñalver <aladaris@gmail.com>
+   This file is part of EmguCV-Projects.
+
+    EmguCV-Projects is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    EmguCV-Projects is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with EmguCV-Projects.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Collections.Generic;
